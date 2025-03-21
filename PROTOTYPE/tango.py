fc20d9e78c00bb2793b6e80c349f73331bc11037
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-21 15:14:28 +0530

    FEAT: recursion works

--- PROTOTYPE/tango.py
+++ PROTOTYPE/tango.py
@@ -1,61 +1,104 @@
+from collections import Counter
+
+result=[]
+six=[i for i in range(6)]
+
 def display(grid):
     l=len(grid)
     for i in range(l):
         if i%6==0:
             print()
         print(grid[i],end=" ")
+    print('\n---------------------------')
 
-def check_row(idx):
+def check_row(grid,idx):
     r=get_row(idx)
+    hash=Counter([grid[i] for i in r])
+    if hash[1]>3 or hash[2]>3:
+        return False
+    return True
 
-def check_col(idx):
+
+def check_col(grid,idx):
     c=get_col(idx)
+    hash=Counter([grid[i] for i in c])
+    if hash[1]>3 or hash[2]>3:
+        return False
+    return True
+    
 
 def get_row(idx):
-    return []
+    n=idx//6
+    return [n*6+i for i in six]
 
-def get_col(col):
-    return []
+def get_col(idx):
+    n=idx%6
+    return [6*i+n for i in six]
 
-
-
-def solver(grid,equal,cross,idx):
-    if grid[i]!=0:
-        for i in range(1,3):
+def solver(grid,equal,cross,idx,locked):
+    global result
+    if idx==36:
+        display(grid)
+        result=grid[:]
+        return
+    if grid[idx]==0:
+        for i in [1,2]:
             grid[idx]=i
+
+            modified_equal=[]
             if idx in equal:
-                for i in equal[idx]:
-                    grid[i]=grid[idx]
+                for j in equal[idx]:
+                    if j not in locked:
+                        grid[j]=grid[idx]
+                        modified_equal.append(j)
+
+            modified_cross=[]
             if idx in cross:
-                for i in cross[idx]:
-                    grid[i]=1 if grid[idx]==2 else 1
-            if check_row(idx) and check_col(idx):
-                solver(grid,equal,cross,idx+1)
-            else:
-                if idx in equal:
-                    for i in equal[idx]:
-                        grid[i]=0
-                if idx in cross:
-                    for i in cross[idx]:
-                        grid[i]=0
+                for j in cross[idx]:
+                    if j not in locked:
+                        grid[j]=2 if grid[idx]==1 else 1
+                        modified_cross.append(j)
+
+            if check_row(grid,idx) and check_col(grid,idx):
+                display(grid)
+                solver(grid,equal,cross,idx+1,locked)
+
+            for j in modified_equal:
+                if j not in locked:
+                    grid[j]=0
+            for j in modified_cross:
+                if j not in locked:
+                    grid[j]=0
+            grid[idx]=0
+    else:
+        if check_row(grid,idx) and check_col(grid,idx):
+            display(grid)
+            solver(grid,equal,cross,idx+1,locked)
         else:
-            solver(grid,equal,cross,idx+1)
+            if idx not in locked:
+                grid[idx]=0
+            return
+
+        
 
 
 if __name__=="__main__":
+    '''Board setup'''
     tango=[0,0,0,0,0,0,
             0,0,0,0,0,1,
             0,0,0,0,1,0,
             0,0,0,1,0,2,
             0,0,1,0,1,0,
             0,1,0,1,0,2]
-    
-    print(tango)
-    display(tango)
-
+    locked=[i for i in range(36) if tango[i]!=0]
     equal={1:[2,7],6:[7,12]}
     cross={}
-    if solver(tango,equal,cross,0):
-        print("found states")
-    else:
-        print("no solution found")
+
+    #print(tango)
+    #display(tango)
+    #print([tango[i] for i in get_row(28)])
+    #print([tango[i] for i in get_col(28)])
+    
+    solver(tango,equal,cross,0,locked)
+    print("RESULT:")
+    display(result)
