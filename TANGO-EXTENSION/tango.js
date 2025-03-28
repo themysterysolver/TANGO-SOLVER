8cc9bcd45c9075c3c878e68fc52cf8e47a7344c9
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-28 21:08:08 +0530

    FEAT:clicking is handled! clickIndex

--- TANGO-EXTENSION/tango.js
+++ TANGO-EXTENSION/tango.js
@@ -3,10 +3,33 @@ document.getElementById("pop").addEventListener("click", async () => {
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: printHello
+        function: solveIt
     });
 });
 
-function printHello() {
-    console.log("Hello");
+function solveIt() {
+    let iframe_coll=document.getElementsByClassName("game-launch-page__iframe w-full")[0];
+    let iframe=iframe_coll.contentDocument||iframe_coll.contentWindow?.document;
+
+    const content=iframe.getElementsByClassName("lotka-cell");
+    const size=Math.sqrt(content.length)
+
+    console.log(content);
+
 }
+function clickIndex(index){
+    let tile=iframe.querySelector(`[data-cell-idx="${index}"]`);
+    if(tile){
+        console.log(tile);
+        function clickElement(element){
+            let down=new MouseEvent("mousedown",{bubbles:true,cancelable:true});
+            let up=new MouseEvent("mouseup",{bubbles:true,cancelable:true});
+            let click=new MouseEvent("click",{bubbles:true,cancelable:true});
+            element.dispatchEvent(down);
+            element.dispatchEvent(up);
+            element.dispatchEvent(click);
+        }
+        clickElement(tile);
+    }
+}
+clickIndex(1);
