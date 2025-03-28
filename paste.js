dba437ac154a1917798158affd63fa86128896ec
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-28 23:04:11 +0530

    FEAT:to be pasted

--- paste.js
+++ paste.js
@@ -1,9 +1,30 @@
- let iframe_coll=document.getElementsByClassName("game-launch-page__iframe w-full")[0];
+let iframe_coll=document.getElementsByClassName("game-launch-page__iframe w-full")[0];
     let iframe=iframe_coll.contentDocument||iframe_coll.contentWindow?.document;
 
     const content=iframe.getElementsByClassName("lotka-cell");
     const size=Math.sqrt(content.length);
 
+    console.log(content);
+    let num_str=new Map();
+    for(let idx=0;idx<content.length;idx++){
+        if(!content[idx].classList.contains("lokta-cell--locked")){
+            await clickIndex(idx);
+            let cell=iframe.querySelector(`[data-cell-idx="${idx}"]`)
+            //console.log("CHECK-1",cell);
+            let label=cell.getElementsByClassName("lotka-cell-content")[0].querySelector("svg").getAttribute("aria-label");
+            num_str.set(1,label);
+
+            await clickIndex(idx);
+            cell=iframe.querySelector(`[data-cell-idx="${idx}"]`);
+            //console.log("CHECK-2:",cell);
+            let nlabel=cell.getElementsByClassName("lotka-cell-content")[0].querySelector("svg").getAttribute("aria-label");
+            num_str.set(2,nlabel);
+
+            clickIndex(idx);
+            break;
+        }
+    }
+    console.log(num_str);
 
     function delay(ms){
         return new Promise(resolve=>setTimeout(resolve,ms));
@@ -12,7 +33,7 @@
         return new Promise(resolve=>{
             let tile=iframe.querySelector(`[data-cell-idx="${index}"]`);
             if(tile){
-                console.log(`Tile clicked at ${index}`);
+                //console.log(`Tile clicked at ${index}`);
                 async function clickElement(element){
                     let down=new MouseEvent("mousedown",{bubbles:true,cancelable:true});
                     let up=new MouseEvent("mouseup",{bubbles:true,cancelable:true});
@@ -20,7 +41,7 @@
                     element.dispatchEvent(down);
                     element.dispatchEvent(up);
                     element.dispatchEvent(click);
-                    await delay(50);
+                    await delay(5);
                     resolve();
                 }
                 clickElement(tile);
@@ -30,27 +51,3 @@
             }
         });
     }
-
-    console.log(content);
-    let num_str=new Map();
-    for(let idx=0;idx<content.length;idx++){
-        if(!content[idx].classList.contains("lokta-cell--locked")){
-            await clickIndex(idx);
-            let cell=iframe.querySelector(`[data-cell-idx="${idx}"]`)
-            console.log("CHECK-1",cell);
-            let label=cell.getElementsByClassName("lotka-cell-content")[0].querySelector("svg").getAttribute("aria-label");
-            num_str.set(1,label);
-
-        
-            await clickIndex(idx);
-            cell=iframe.querySelector(`[data-cell-idx="${idx}"]`);
-            console.log("CHECK-2:",cell);
-            let nlabel=cell.getElementsByClassName("lotka-cell-content")[0].querySelector("svg").getAttribute("aria-label");
-            num_str.set(2,nlabel);
-
-            //clickIndex(idx);
-            break;
-        }
-    }
-    console.log(num_str);
-
